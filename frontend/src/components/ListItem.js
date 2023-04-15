ed1d66e9439df140222089bfc12a251ab9a57c77
giuffridavide <eng.davidegiuffrida@gmail.com>
2023-04-15 11:23:47 +0200

    Very basic components and pages for trips

---
+++ frontend/src/components/ListItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListItesm = ({trip}) => {
+  return (
+    <div>
+        <h3>{trip.main_destination}</h3>
+    </div>
+  )
+}
+
+export default ListItesm
--- frontend/src/pages/TripsListPage.js
+++ frontend/src/pages/TripsListPage.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import ListItem from '../components/ListItem'
 
 const TripsListPage = () => {
-  return (
-    <div>
-        trips
-    </div>
-  )
+
+    let [trips, setTrip] = useState([])
+
+    useEffect(()=> {
+        getTrips()
+    }, [])
+
+    // await means let's wait until you don't have the response
+    let getTrips = async () =>{
+        let response = await fetch('/api/trips/')
+        let data = await response.json()
+        setTrip(data)
+    }
+
+    return (
+        <div>
+            <div className='trips-list'>
+                {trips.map((trip, index) => (
+                    <ListItem key={index} trip={trip} />
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default TripsListPage
