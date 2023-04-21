10a1e88e8b948780e7e7ae312e08e1e6aef55e25
giuffridavide <eng.davidegiuffrida@gmail.com>
2023-04-22 01:37:03 +0200

    WIP Dashboard-TripDetails-DisplayTrip

--- frontend/src/components/DisplayTrip.js
+++ frontend/src/components/DisplayTrip.js
@@ -1,39 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from "react-router-dom";
+import TripDetailsPage from "../pages/TripDetailsPage";
 
-function compareDates(d1, d2){
-  let today = new Date(d1).getTime();
-  let trip_date = new Date(d2).getTime();
-
-  let res = null;
-
-  if (trip_date <= today) {
-    res = true;
-  }
-  else{
-    res = false;
-  }
-
-  return res;
-}
 
 const DisplayTrip = ({trip}) => {
+
   const imagePath = `images/trips/${trip.destination.name}.jpg`;
   
-  const date = new Date();
-  let day = date.getDate();
-  let month = date.getMonth() + 1;
-  let year = date.getFullYear();
-  let currentDate = `${year}-${month}-${day}`;
-
-  const past = compareDates(currentDate, trip.end_date);
-  const classDivPast = past? `trip-card past-trip` : `trip-card`;
   return (
-    <div className={classDivPast}>
-      <img src={imagePath} alt={trip.destination.name}></img>
-      <h2>{trip.destination.name}</h2>
-      <h3>{trip.start_date}</h3>
-      <p>{trip.destination.punchline}</p>
-      <a href="#">View Details</a>
+    <div>
+      <div className='trip-card'>
+          <img src={imagePath} alt={trip.destination.name}></img>
+          <h2>{trip.destination.name}</h2>
+          <h3>{trip.start_date}</h3>
+          <p>{trip.destination.punchline}</p>
+          <Link to={{pathname:'/tripDetails', state: trip}}>
+            <button>View Details</button>
+          </Link>
+        </div>
     </div>
   )
 }
