7b9229cfd6563750196a0f8ac831a59e08bf4835
giuffridavide <eng.davidegiuffrida@gmail.com>
2023-04-23 23:09:43 +0200

    Added docker

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 pipenv run python manage.py flush --no-input
-pipenv run python convert_fixtures.py
 pipenv run python manage.py makemigrations
 pipenv run python manage.py migrate
+pipenv run python convert_fixtures.py
+pipenv run python manage.py loaddata dump.json
 pipenv run python manage.py createsuperuser --no-input
 pipenv run python manage.py runserver 0.0.0.0:8000
